2145b82e2bd28e4d18cdf44408a251eeb6667b44
reza <yadegarireza50@googlemail.com>
2024-11-23 22:48:45 +0330

    cls methodes for update some features

--- inventory_psycopg.py
+++ inventory_psycopg.py
@@ -9,7 +9,7 @@ class InventoryConnection:
         if cls.connection_inventory:
             return cls.connection_inventory
         else:
-            cls.connection_inventory = psycopg2.connect(database="inventory_psycopg", user='postgres', password='1234',
+            cls.connection_inventory = psycopg2.connect(database="inventory_management", user='postgres', password='1234',
                                                         host='127.0.0.1', port='5432')
             cls.connection_inventory.autocommit = True
             return cls.connection_inventory
@@ -20,14 +20,14 @@ class InventoryConnection:
         return cursor
 
     @classmethod
-    def create_table(cls):
-        query = """CREATE TABLE inventory(id SERIAL PRIMARY KEY,
+    def create_table(cls, table_name : str):
+        query = """CREATE TABLE {}(id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
 	QUANTITY INT,
-	PRICE DECIMAL(10,2))"""
+	PRICE DECIMAL(10,2))""".format(table_name)
         db_cursor = cls.get_cursor()
         db_cursor.execute(query)
-        print("TABLE CREATE WAS SUCCESSFUL")
+        return f"TABLE CREATE WAS SUCCESSFUL"
     @classmethod
     def get_all(cls, table_name: str) -> List[Tuple]:
         """the function return all TABLE INFO list
@@ -38,11 +38,18 @@ class InventoryConnection:
         db_cursor.execute(query)
         item_list = db_cursor.fetchall()
         return item_list
-
+    @classmethod
+    def update_filesize(cls, filesize, id):
+        query = """update inventory 
+set file_size = {} where id = {}""".format(filesize,id)
+        db_cursor = cls.get_cursor()
+        db_cursor.execute(query)
 
 
 
 if __name__ == '__main__':
- #   inventory_instance = InventoryConnection.create_table()
+#    inventory_instance = InventoryConnection.create_table('asgharagha')
+    InventoryConnection.update_filesize(333.33,3)
     list_ = InventoryConnection.get_all('inventory')
-    print(list_)
+    for product in list_:
+        print(product,'\n')
